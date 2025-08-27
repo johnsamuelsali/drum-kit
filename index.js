0bae260771184d22df168774ef221405e622fdd3
John Samuel <johnsamuelsali@gmail.com>
2025-08-27 19:28:47 +0530

    Created a drum kit website

---
+++ index.js
@@ -0,0 +1,56 @@
+var drums = document.querySelectorAll(".drum");
+
+for (var i = 0; i < drums.length; i++) {
+  drums[i].addEventListener("click", function() {
+    playSound(this.innerHTML);
+     animate(this.innerHTML);
+  });
+}
+
+
+document.addEventListener("keydown", function (KeyboardEvent) {
+    playSound(KeyboardEvent.key);
+     animate(KeyboardEvent.key);
+});
+
+
+function playSound(key) {
+    switch (key) {
+      case "a":
+        var tom1 = new Audio("./sounds/tom-1.mp3");
+        tom1.play();
+        break;
+      case "s":
+        var tom2 = new Audio("./sounds/tom-2.mp3");
+        tom2.play();
+        break;
+      case "d":
+        var tom3 = new Audio("./sounds/tom-3.mp3");
+        tom3.play();
+        break;
+      case "f":
+        var tom4 = new Audio("./sounds/tom-4.mp3");
+        tom4.play();
+        break;
+      case "j":
+        var snare = new Audio("./sounds/snare.mp3");
+        snare.play();
+        break;
+      case "k":
+        var crash = new Audio("./sounds/crash.mp3");
+        crash.play();
+        break;
+      case "l":
+        var bass = new Audio("./sounds/kick-bass.mp3");
+        bass.play();
+        break;
+    }
+  }
+
+function animate(key) {
+    var activeButton = document.querySelector("." +key);
+    activeButton.classList.add("pressed");
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
